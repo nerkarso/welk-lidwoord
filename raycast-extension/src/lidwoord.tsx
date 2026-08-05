f838682f9708474e17e6da924509e47f2638b2de
Ner Karso <nerpkarso@gmail.com>
2026-08-05 12:19:20 -0300

    feat: add history and search functionality

--- raycast-extension/src/lidwoord.tsx
+++ raycast-extension/src/lidwoord.tsx
@@ -1,39 +1,55 @@
-import { Action, ActionPanel, Detail } from '@raycast/api';
+import { Action, ActionPanel, Detail, Keyboard, showToast, Toast } from '@raycast/api';
 import { useEffect, useState } from 'react';
+import { getSearchByWord } from './api';
+import { createLidwoordMarkdown } from './utils';
 
 type Props = {
-	arguments: {
-		word: string;
-	};
+  arguments: {
+    word: string;
+  };
 };
 
 export default function Command(props: Props) {
-	const [result, setResult] = useState<string>();
+  const [result, setResult] = useState<string>();
+  const [error, setError] = useState(false);
+  const word = props.arguments.word.trim();
 
-	useEffect(() => {
-		const fetchData = async () => {
-			const response = await fetch(
-				`http://localhost:8806/search/${props.arguments.word}`,
-			);
-			const data = await response.text();
-			setResult(data);
-		};
-		fetchData();
-	}, []);
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data, error } = await getSearchByWord({
+        path: { word },
+      });
 
-	return (
-		<Detail
-			navigationTitle={`De of het ${props.arguments.word}?`}
-			isLoading={result === undefined}
-			markdown={`# ${result || 'Searching...'}`}
-			actions={
-				<ActionPanel>
-					<Action.CopyToClipboard
-						content={result || ''}
-						shortcut={{ modifiers: ['cmd'], key: '.' }}
-					/>
-				</ActionPanel>
-			}
-		/>
-	);
+      if (error) {
+        throw new Error('Search request failed');
+      }
+
+      setResult(data);
+    };
+    fetchData().catch(async () => {
+      setError(true);
+      await showToast({
+        style: Toast.Style.Failure,
+        title: 'Could not search for word',
+      });
+    });
+  }, []);
+
+  return (
+    <Detail
+      navigationTitle={`Lidwoord voor ${word}`}
+      isLoading={result === undefined && !error}
+      markdown={createLidwoordMarkdown(word, result, error)}
+      actions={
+        <ActionPanel>
+          <Action.CopyToClipboard
+            content={result ? result : ''}
+            title="Copy Article and Word"
+            shortcut={Keyboard.Shortcut.Common.Copy}
+          />
+          <Action.CopyToClipboard content={result?.split(' ')?.[0] ?? ''} title="Copy Article" />
+        </ActionPanel>
+      }
+    />
+  );
 }
